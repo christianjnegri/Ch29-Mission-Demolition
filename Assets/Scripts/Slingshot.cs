782453171cb856d53dfed1362dba8dd93e90b255
christianjnegri <christianjnegri@gmail.com>
2016-06-18 16:06:50 -0400

    Projectile Launch
    Code added to make projectile appear and launch from the slingshot.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,13 +3,56 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	public bool ______________;
+	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+
+	void Update () {
+		if (!aimingMode)
+			return;
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+		if (Input.GetMouseButtonUp (0)) {
+			aimingMode = false;
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			projectile = null;
+		}
+	}
+	void Awake() {
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+	}
 	// Use this for initialization
-	void Start () {
-	
+	void OnMouseEnter () {
+		print ("Slingshot:OnMouseEnter");
+		launchPoint.SetActive (true);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseExit () {
+		print ("Slingshot:OnMouseExit");
+		launchPoint.SetActive (false);
+	}
+	void OnMouseDown() {
+		aimingMode = true;
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.rigidbody.isKinematic = true;
 	}
 }
