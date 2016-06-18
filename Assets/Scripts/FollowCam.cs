36327013d95f41b2abe178e71f3725f499659f5c
christianjnegri <christianjnegri@gmail.com>
2016-06-18 16:59:31 -0400

    Camera Follow
    The camera now follows the projectile.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S;
+	public float easing = 0.05f;
+	public Vector2 minXY;
+	public bool _________________;
+	public GameObject poi;
+	public float camZ;
+	void Start () {
+		S = this;
+		camZ = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (poi == null)
+			return;
+		Vector3 destination = poi.transform.position;
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		destination = Vector3.Lerp(transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		this.camera.orthographicSize = destination.y + 10;
+	}
+}
